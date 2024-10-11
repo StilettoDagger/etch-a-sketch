3e8a166c2e33c7c9d6eea9eb25ce75072d0eac05
StilettoDagger <159158452+StilettoDagger@users.noreply.github.com>
2024-10-11 18:49:10 +0300

    Fix: fix bug where row elements are created instead of div elements

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const sketchCanvas = document.querySelector("#sketch");
 // Create a 16x16 grid of divs for the canvas
 function createCanvas() {
     for (let i = 0; i < 16; i++) {
-        // Create 16 columns
+        // Create 16 div columns
         const newColumn = document.createElement("div");
         newColumn.classList.add("col");
         sketchCanvas.appendChild(newColumn);
         for (let j = 0; j < 16; j++) {
             // For every column, create 16 rows of divs
-            const newRow = document.createElement("row");
+            const newRow = document.createElement("div");
             newRow.classList.add("row");
             newColumn.appendChild(newRow);
         }
