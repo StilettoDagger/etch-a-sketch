82a57107ce938f837e105ad7e8571716dfe4fba1
StilettoDagger <159158452+StilettoDagger@users.noreply.github.com>
2024-10-12 15:40:59 +0300

    Format: fix code formatting for all files

--- script.js
+++ script.js
@@ -6,75 +6,65 @@ const sketchCanvas = document.querySelector("#sketch-container");
  * @param {number} height - The height of the canvas which is equivalent to the number of cells in each column.
  */
 function createCanvas(width, height) {
-    for (let i = 0; i < width; i++) {
-        // Create div columns based on the width parameter.
-        const newColumn = document.createElement("div");
-        newColumn.classList.add("col");
-        sketchCanvas.appendChild(newColumn);
-        for (let j = 0; j < height; j++) {
-            // For every column, create 16 cell divs
-            const newCell = document.createElement("div");
-            newCell.classList.add("cell");
-            newColumn.appendChild(newCell);
-        }
-    }
+	for (let i = 0; i < width; i++) {
+		// Create div columns based on the width parameter.
+		const newColumn = document.createElement("div");
+		newColumn.classList.add("col");
+		sketchCanvas.appendChild(newColumn);
+		for (let j = 0; j < height; j++) {
+			// For every column, create 16 cell divs
+			const newCell = document.createElement("div");
+			newCell.classList.add("cell");
+			newColumn.appendChild(newCell);
+		}
+	}
 
-    createCellHoverEffect();
+	createCellHoverEffect();
 }
 
 // Change the color of the cells that are hovered on with the mouse.
 function createCellHoverEffect() {
-    const cells = document.querySelectorAll(".cell");
+	const cells = document.querySelectorAll(".cell");
 
-    for (const cell of cells) {
-        cell.addEventListener("mouseenter", e => {
-            e.target.classList.add("active");
-        })
-    }
+	for (const cell of cells) {
+		cell.addEventListener("mouseenter", (e) => {
+			e.target.classList.add("active");
+		});
+	}
 }
 
-// Function for removing all canvas cells from the current canvas.
 function removeCanvas() {
-    sketchCanvas.innerHTML = "";
+	sketchCanvas.innerHTML = "";
 }
 
-// Event handler function for creating a canvas grid.
+// Event handler function for creating a new canvas grid.
 function createNewCanvas(e) {
-    let isValid = false;
-    let userInput = null;
-    do {
-        userInput = prompt("Please enter the number of squares per side for your new canvas (<100): ", "16");
+	let isValid = false;
+	let userInput = null;
+	do {
+		userInput = prompt(
+			"Please enter the number of squares per side for your new canvas (<100): ",
+			"16"
+		);
 
-        if (userInput === null) return;
+		if (userInput === null) return;
 
-        if (isNaN(userInput))
-        {
-            alert("Please enter a valid number.");
-        }
+		if (isNaN(userInput)) {
+			alert("Please enter a valid number.");
+		} else if (userInput >= 100) {
+			alert("Please enter a number less than 100");
+		} else if (userInput <= 0) {
+			alert("Please enter a positive number");
+		} else {
+			isValid = true;
+		}
+	} while (!isValid);
 
-        else if (userInput >= 100)
-        {
-            alert("Please enter a number less than 100")
-        }
+	removeCanvas();
 
-        else if (userInput <= 0)
-        {
-            alert("Please enter a positive number");
-        }
+	const cellCount = Number(userInput);
 
-        else
-        {
-            isValid = true;
-        }
-
-    } while(!isValid);
-
-    removeCanvas();
-
-    const cellCount = Number(userInput);
-
-    createCanvas(cellCount, cellCount);
-
+	createCanvas(cellCount, cellCount);
 }
 
 document.querySelector("#new-grid").addEventListener("click", createNewCanvas);
