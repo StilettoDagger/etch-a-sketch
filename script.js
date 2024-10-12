e0a7419962c54c0af3a33299c70d763e1d699314
StilettoDagger <159158452+StilettoDagger@users.noreply.github.com>
2024-10-12 15:15:23 +0300

    Feat: add functionality that allows the user to create a new square canvas of a specific size

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 const sketchCanvas = document.querySelector("#sketch-container");
 
-// Create a 16x16 grid of divs for the canvas
-function createCanvas() {
-    for (let i = 0; i < 16; i++) {
-        // Create 16 div columns
+/**
+ * Create a grid of div cells based on the specified size for the width and height of the canvas.
+ * @param {number} width - The width of the canvas which is equivalent to the number of cell columns.
+ * @param {number} height - The height of the canvas which is equivalent to the number of cells in each column.
+ */
+function createCanvas(width, height) {
+    for (let i = 0; i < width; i++) {
+        // Create div columns based on the width parameter.
         const newColumn = document.createElement("div");
         newColumn.classList.add("col");
         sketchCanvas.appendChild(newColumn);
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < height; j++) {
             // For every column, create 16 cell divs
             const newCell = document.createElement("div");
             newCell.classList.add("cell");
             newColumn.appendChild(newCell);
         }
     }
+
+    createCellHoverEffect();
 }
 
-// Change the color of the cells that are hovered on with the mouse
+// Change the color of the cells that are hovered on with the mouse.
 function createCellHoverEffect() {
     const cells = document.querySelectorAll(".cell");
 
@@ -27,6 +33,51 @@ function createCellHoverEffect() {
     }
 }
 
-createCanvas();
+// Function for removing all canvas cells from the current canvas.
+function removeCanvas() {
+    const sketchCols = sketchCanvas.childNodes;
 
-createCellHoverEffect();
+    for (const col of sketchCols) {
+        col.remove();
+    }
+}
+
+// Event handler function for creating a canvas grid.
+function createNewCanvas(e) {
+    let isValid = false;
+    let userInput = null;
+    do {
+        userInput = prompt("Please enter the number of squares per side for your new canvas (<100): ", "16");
+        if (isNaN(userInput))
+        {
+            alert("Please enter a valid number.");
+        }
+
+        else if (userInput >= 100)
+        {
+            alert("Please enter a number less than 100")
+        }
+
+        else if (userInput <= 0)
+        {
+            alert("Please enter a positive number");
+        }
+
+        else
+        {
+            isValid = true;
+        }
+
+    } while(!isValid);
+
+    removeCanvas();
+
+    const cellCount = Number(userInput);
+
+    createCanvas(cellCount, cellCount);
+
+}
+
+document.querySelector("#new-grid").addEventListener("click", createNewCanvas);
+
+createCanvas(16, 16);
